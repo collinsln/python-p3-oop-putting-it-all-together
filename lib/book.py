f4a2d9ec00fd9a297794dcf013a6461d9e70b94c
collinsln <collangat@gmail.com>
2023-12-03 09:37:58 +0300

    else

--- lib/book.py
+++ lib/book.py
@@ -6,6 +6,11 @@ class Book:
         if not isinstance(page_count, int):
             print("page_count must be an integer")
             self.page_count = None
+        else:
+            self.page_count = page_count
+    def turn_page(self):
+            print("Flipping the page...wow, you read fast!")
+
  
     
         
