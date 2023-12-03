1abc325b3ea9531ddac84045a57b4ac0a2161d4a
collinsln <collangat@gmail.com>
2023-12-03 09:36:46 +0300

    econd

--- lib/book.py
+++ lib/book.py
@@ -3,6 +3,9 @@
 class Book:
     def __init__(self,title,page_count):
         self.title = title 
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            self.page_count = None
  
     
         
